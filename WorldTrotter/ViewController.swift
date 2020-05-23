0100624a03b4e8dba5171f4c523a52b9b02e1bd3
Glenn Cole <gcole@mac.com>
2020-05-23 12:47:34 -0700

    Initially, play with dynamic views

--- WorldTrotter/ViewController.swift
+++ WorldTrotter/ViewController.swift
@@ -12,9 +12,23 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        playWithDynamicViews()
     }
 
-
+    private func playWithDynamicViews() {
+        
+        let firstFrame = CGRect( x: 160, y: 240, width: 100, height: 150 )
+        let firstView = UIView( frame: firstFrame )
+        firstView.backgroundColor = .lightGray
+        view.addSubview( firstView )
+        
+        let secondFrame = CGRect( x: 20, y: 30, width: 50, height: 50 )
+        let secondView = UIView( frame: secondFrame )
+        secondView.backgroundColor = .blue
+        
+        view.addSubview( secondView )  // views can have only one parent, so overridden!
+        firstView.addSubview( secondView )
+    }
 }
 
