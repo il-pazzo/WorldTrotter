e88c54157722cac7efdbc0dfb7e65c660db3a745
Glenn Cole <gcole@mac.com>
2020-05-23 12:32:54 -0700

    Initial Commit

---
+++ WorldTrotter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Glenn Cole on 5/23/20.
+//  Copyright © 2020 Glenn Cole. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
