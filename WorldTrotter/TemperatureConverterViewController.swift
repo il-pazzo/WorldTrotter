5b6b9595045d89863536357dd85a5bcf4cf76827
Glenn Cole <gcole@mac.com>
2020-05-23 17:37:14 -0700

    Removed dynamic view "play" code Added storyboard skeleton for temperature converter

---
+++ WorldTrotter/TemperatureConverterViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Glenn Cole on 5/23/20.
+//  Copyright © 2020 Glenn Cole. All rights reserved.
+//
+
+import UIKit
+
+class TemperatureConverterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
