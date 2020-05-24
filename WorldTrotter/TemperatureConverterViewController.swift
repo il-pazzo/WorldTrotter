017680be14c5cfee17563f363423e45a3b76254f
Glenn Cole <gcole@mac.com>
2020-05-23 22:54:52 -0700

    Converter now functional: -- accepts input -- converts to celsius -- formats the resulting value -- dismisses keyboard with a tap outside of the field

--- WorldTrotter/TemperatureConverterViewController.swift
+++ WorldTrotter/TemperatureConverterViewController.swift
@@ -10,10 +10,67 @@ import UIKit
 
 class TemperatureConverterViewController: UIViewController {
 
+    @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField: UITextField!
+    
+    let NO_CELSIUS_VALUE = "???"
+    
+    var fahrenheitValue: Measurement<UnitTemperature>? {
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+    
+    var celsiusValue: Measurement<UnitTemperature>? {
+        if let fahrenheitValue = fahrenheitValue {
+            return fahrenheitValue.converted( to: .celsius )
+        }
+        else {
+            return nil
+        }
+    }
+    
+    let celsiusFormatter: NumberFormatter = {
+        let fmt = NumberFormatter()
+        fmt.numberStyle = .decimal
+        fmt.minimumFractionDigits = 0
+        fmt.maximumFractionDigits = 1
+        return fmt
+    }()
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         print( "TemperatureConverterViewController loaded its view" )
+        
+        updateCelsiusLabel()
+    }
+    
+    @IBAction func fahrenheitFieldChanged( _ textField: UITextField ) {
+        
+        if let text = textField.text, let value = Double(text) {
+            fahrenheitValue = Measurement( value: value, unit: .fahrenheit )
+        }
+        else {
+            fahrenheitValue = nil
+        }
+    }
+    
+    @IBAction func dismissKeyboard( _ sender: UITapGestureRecognizer ) {
+        
+        textField.resignFirstResponder()
+    }
+    
+    func updateCelsiusLabel() {
+        if let celsiusValue = celsiusValue {
+            celsiusLabel.text = celsiusFormatter.string( from:
+                NSNumber( value: celsiusValue.value )
+            )
+        }
+        else {
+            celsiusLabel.text = NO_CELSIUS_VALUE
+        }
     }
 }
 
